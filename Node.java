ad67fd50031013d9bd39b08aeec26f34655d8db2
Daniel Henrique Carvalho <90934371+DanielHQ1@users.noreply.github.com>
2026-09-13 17:52:25 +0000

    comeco projeto

---
+++ Node.java
@@ -0,0 +1,142 @@
+//Integrantes do Grupo:
+//Nome: Daniel Henrique Carvalho RA: 10736657
+//Nome: Felipe Sousa Vasques Honorato RA: 10735577
+//Referencias: https://graduacao.mackenzie.br/pluginfile.php/1292281/mod_resource/content/1/Lab1b%20-%20BST%20-%20Sem%20Entrega.pdf , https://graduacao.mackenzie.br/pluginfile.php/1342943/mod_resource/content/0/Aula%20-%20%C3%81rvore%20Bin%C3%A1ria.pdf
+ 
+
+
+public class Node {
+    private String data;
+
+    private Node a;
+    private Node b;
+    private Node c;
+    private Node d;
+    private Node e;
+    private Node f;
+    private Node g;
+    private Node h;
+    private Node i;
+    private Node j;
+    private Node k;
+    private Node l;
+    private Node m;
+    private Node n;
+    private Node o;
+    private Node p;
+    private Node q;
+    private Node r;
+    private Node s;
+    private Node t;
+    private Node u;
+    private Node v;
+    private Node w;
+    private Node x;
+    private Node y;
+    private Node z;
+
+    private boolean isWord;
+    private boolean hasNextWord;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Node getA() { return a; }
+    public void setA(Node a) { this.a = a; }
+
+    public Node getB() { return b; }
+    public void setB(Node b) { this.b = b; }
+
+    public Node getC() { return c; }
+    public void setC(Node c) { this.c = c; }
+
+    public Node getD() { return d; }
+    public void setD(Node d) { this.d = d; }
+
+    public Node getE() { return e; }
+    public void setE(Node e) { this.e = e; }
+
+    public Node getF() { return f; }
+    public void setF(Node f) { this.f = f; }
+
+    public Node getG() { return g; }
+    public void setG(Node g) { this.g = g; }
+
+    public Node getH() { return h; }
+    public void setH(Node h) { this.h = h; }
+
+    public Node getI() { return i; }
+    public void setI(Node i) { this.i = i; }
+
+    public Node getJ() { return j; }
+    public void setJ(Node j) { this.j = j; }
+
+    public Node getK() { return k; }
+    public void setK(Node k) { this.k = k; }
+
+    public Node getL() { return l; }
+    public void setL(Node l) { this.l = l; }
+
+    public Node getM() { return m; }
+    public void setM(Node m) { this.m = m; }
+
+    public Node getN() { return n; }
+    public void setN(Node n) { this.n = n; }
+
+    public Node getO() { return o; }
+    public void setO(Node o) { this.o = o; }
+
+    public Node getP() { return p; }
+    public void setP(Node p) { this.p = p; }
+
+    public Node getQ() { return q; }
+    public void setQ(Node q) { this.q = q; }
+
+    public Node getR() { return r; }
+    public void setR(Node r) { this.r = r; }
+
+    public Node getS() { return s; }
+    public void setS(Node s) { this.s = s; }
+
+    public Node getT() { return t; }
+    public void setT(Node t) { this.t = t; }
+
+    public Node getU() { return u; }
+    public void setU(Node u) { this.u = u; }
+
+    public Node getV() { return v; }
+    public void setV(Node v) { this.v = v; }
+
+    public Node getW() { return w; }
+    public void setW(Node w) { this.w = w; }
+
+    public Node getX() { return x; }
+    public void setX(Node x) { this.x = x; }
+
+    public Node getY() { return y; }
+    public void setY(Node y) { this.y = y; }
+
+    public Node getZ() { return z; }
+    public void setZ(Node z) { this.z = z; }
+
+    public boolean isWord() {
+        return isWord;
+    }
+
+    public void setWord(boolean word) {
+        isWord = word;
+    }
+
+    public boolean isHasNextWord() {
+        return hasNextWord;
+    }
+
+    public void setHasNextWord(boolean hasNextWord) {
+        this.hasNextWord = hasNextWord;
+    }
+}
